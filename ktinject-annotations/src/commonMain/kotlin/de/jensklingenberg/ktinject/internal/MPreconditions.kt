64f7cf48157e54c639e0917de290961697e9d51f
Jens Klingenberg <mail@jensklingenberg.de>
2020-01-07 00:09:19 +0100

    sort parameters

--- ktinject-annotations/src/commonMain/kotlin/de/jensklingenberg/ktinject/internal/MPreconditions.kt
+++ ktinject-annotations/src/commonMain/kotlin/de/jensklingenberg/ktinject/internal/MPreconditions.kt
@@ -2,6 +2,15 @@ package de.jensklingenberg.ktinject.internal
 
 class MPreconditions{
    companion object{
+
+       /**
+        * Ensures that an object reference passed as a parameter to the calling method is not null.
+        *
+        * @param reference an object reference
+        * @param errorMessage the exception message to use if the check fails
+        * @return the non-null reference that was validated
+        * @throws NullPointerException if {@code reference} is null
+        */
        fun <T> checkNotNull(reference: T?, errorMessage: String?=""): T {
            if (reference == null) {
                throw NullPointerException(errorMessage)
